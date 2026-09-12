e6bd41bf3ebfe89d88a71692beea2f5185e2774a
Jose <luissms@gmail.com>
2026-09-12 15:27:35 -0500

    actualizacion de application.yml fecha local, y main

--- src/main/java/com/wms/backend/WmsBackendApplication.java
+++ src/main/java/com/wms/backend/WmsBackendApplication.java
@@ -1,11 +1,17 @@
 package com.wms.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class WmsBackendApplication {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WmsBackendApplication.class, args);
 	}
